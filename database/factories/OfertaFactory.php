66a5a7050b7afb48e434fd0fe107283350f4646f
vagrant <vagrant@homestead>
2020-03-22 19:37:27 +0000

    actualiza 682

--- database/factories/OfertaFactory.php
+++ database/factories/OfertaFactory.php
@@ -6,14 +6,17 @@ use Faker\Generator as Faker;
 use App\Models\Oferta\Oferta;
 
 $factory -> define( Oferta::class, function (Faker $faker) {
+    $name = $faker->sentence(4, true);
+    $descripcion = $faker->sentence(6, true);
+    $precio = $faker->randomFloat(2, 10, 100);
     return [
-            'user_id' => rand(1, 10),
-            'name' => $faker->sentence(4, true),
-            'unidad' => $faker->word,
-            'descripcion' => $faker->sentence(6, true),
-            'stock' => rand(1, 10),
-            'precio' => $faker->randomFloat(2, 10, 100),
-            'file' => 'oferta.png',
-            'status' => true
+            'user_id'     => rand(1, 10),
+            'name'        => $name,
+            'unidad'      => $faker->word,
+            'descripcion' => $descripcion,
+            'stock'       => rand(1, 10),
+            'precio'      => $precio,
+            'file'        => 'oferta.png',
+            'status'      => true
     ];
 });
