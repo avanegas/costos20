891a17c51868052dbc818b182bccb33b5d21249d
avanegas <vanegasalfredo@gmail.com>
2021-01-03 08:52:03 -0500

    actualizacion laravel 8.20.1

--- public/js/app.js
+++ public/js/app.js
@@ -79216,8 +79216,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/vagrant/Projects/costos20/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/vagrant/Projects/costos20/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\costos20\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\costos20\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
